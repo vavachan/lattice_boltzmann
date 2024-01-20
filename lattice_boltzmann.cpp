d85068437e483165b1ada9380df7286c985b429c
varghese <vargheseiam@gmail.com>
2024-01-20 08:35:42 -0500

    fixed one bug

--- lattice_boltzmann.cpp
+++ lattice_boltzmann.cpp
@@ -65,6 +65,7 @@ int main()
 
 	float cylinder_radius = charphysL/L;
 	int timesteps = 100000;
+	int write_after = 5000;
 
 	std::cout<<L/delta_t<<"\t"<<delta_t<<"\t"<<cylinder_radius<<"\n";;
 	
@@ -77,9 +78,9 @@ int main()
 	//float k=1;	
 	//float T=1.;
 			       //
-	std::cout<<c<<"\n";
+	//std::cout<<c<<"\n";
 
-	float w = 1/tau; // this is related to updating of the distribution in the collision step
+	float w = 1./tau; // this is related to updating of the distribution in the collision step
 
 	// lattice connectivity 
 	int n_connect = 9;
@@ -119,18 +120,27 @@ int main()
 	// with a radius 1. 
 	int xo=int(1./5.*size_x);
 	int yo=int(1./2.*size_y);
+	int n_solid=0;
 	for(int x=0; x<size_x; x++)
 	{
-		for(int y=0; y<size_x; y++)
+		for(int y=0; y<size_y; y++)
 		{
-			if((x-xo)*(x-xo)+(y-yo)*(y-yo)<pow(cylinder_radius,2))
+			if((x-xo)*(x-xo)+(y-yo)*(y-yo)<=pow(cylinder_radius,2))
 			{
 				//std::cout<<x<<"\t"<<y<<"\n";
 				solid[y*size_x+x]=1.;
+				n_solid = n_solid+1;
+
 			}	
 		}
 	}
+	
+//	for(int y=0;y<size_y;y++)
+//	{
+//		solid[y*size_x+size_x/2]=1.;
+//	}
 
+	std::cout<<n_solid<<"\n";
 	// n_connect is the number of velocities in the system. 
 	
 	// the following are the vectors which corresponds to the 
@@ -195,18 +205,32 @@ int main()
 	weights[8]=1./36.;
 
 
-	for(int n=0; n<n_connect; n++)
+	for(int x=0; x<size_x; x++)
 	{
-		for(int x=0; x<size_x; x++)
+		//int x=0;
+		for(int y=0; y<size_y; y++)
 		{
-			for(int y=0; y<size_y; y++)
+			if(solid[y*size_x+x]==0)
 			{
-				float e_dot_u = e_vec_x[n]*uox+e_vec_y[n]*uoy;
-				float u_mod_sq = uox*uox+uoy*uoy;
-				densities[n*size_x*size_y+y*size_x+x]=weights[n]*(1.+(3./c)*e_dot_u+(9./2.)*(e_dot_u/c)*(e_dot_u/c)-(3./2.)*u_mod_sq/(c*c));
+				for(int n=0; n<n_connect; n++)
+				{
+					float e_dot_u = e_vec_x[n]*uox+e_vec_y[n]*uoy;
+					float u_mod_sq = uox*uox+uoy*uoy;
+					//if(x==size_x/2)
+					{
+						densities[n*size_x*size_y+y*size_x+x]=weights[n]*(1.+3.*e_dot_u+(9./2.)*(e_dot_u)*(e_dot_u)-(3./2.)*u_mod_sq);
+					}
+				//	else
+				//	{
+				//		densities[n*size_x*size_y+y*size_x+x]=weights[n];
+				//	}
+
+					//std::cout<<n<<"\t"<<weights[n]*(1.+(3./c)*e_dot_u+(9./2.)*(e_dot_u/c)*(e_dot_u/c)-(3./2.)*u_mod_sq/(c*c))<<"\n";
+				}
 			}
 		}
 	}
+	//
 	// we need a few more arrays to store the average velocity 
 	// and the density
 
@@ -219,6 +243,28 @@ int main()
 	std::fstream a_vel;
 	std::fstream den_file;
 	std::string filename;
+
+	//std::cout<<t<<"\n";
+	int t=-1;
+	filename = "average_velocity_"+std::to_string(t)+".dat";
+	a_vel.open(filename,std::ios::out);
+	filename = "densities_"+std::to_string(t)+".dat";
+	den_file.open(filename,std::ios::out);
+	for(int x=0; x<size_x; x++)
+	{
+		for(int y=0; y<size_y; y++)
+		{
+			den_file<<x<<"\t"<<y<<"\t";
+			for(int n=0;n<n_connect; n++)
+			{
+				den_file<<densities[n*size_x*size_y+y*size_x+x]-weights[n]<<"\t";
+			}
+			den_file<<"\n";
+			a_vel<<x<<"\t"<<y<<"\t"<<sqrt(ux[y*size_x+x]*ux[y*size_x+x]+uy[y*size_x+x]*uy[y*size_x+x])<<"\t"<<ux[y*size_x+x]<<"\t"<<uy[y*size_x+x]<<"\n";
+		}
+	}
+	a_vel.close();
+	den_file.close();
 	for(int t=0; t<timesteps;t++)
 	{
 		for(int x=0; x<size_x; x++)
@@ -248,32 +294,24 @@ int main()
 					{
 						float e_dot_u = e_vec_x[n]*ux[y*size_x+x]+e_vec_y[n]*uy[y*size_x+x];
 						float u_mod_sq = ux[y*size_x+x]*ux[y*size_x+x]+uy[y*size_x+x]*uy[y*size_x+x];
-						eq_densities[n*size_x*size_y+y*size_x+x]=weights[n]*(1.+(3./c)*e_dot_u+(9./2.)*(e_dot_u/c)*(e_dot_u/c)-(3./2.)*u_mod_sq/(c*c));
+						eq_densities[n*size_x*size_y+y*size_x+x]=rho[y*size_x+x]*weights[n]*(1.+3.*e_dot_u+(9./2.)*(e_dot_u)*(e_dot_u)-(3./2.)*u_mod_sq);
 
 						// now that we have the equilibrium densities we can update the distributions. 
 
 						temp_densities[n*size_x*size_y+y*size_x+x] = densities[n*size_x*size_y+y*size_x+x] + w*(eq_densities[n*size_x*size_y+y*size_x+x]-densities[n*size_x*size_y+y*size_x+x]);
 					}
 				}
-			}
-		}
-		if(t%1000==0)
-		{
-			std::cout<<t<<"\n";
-			filename = "average_velocity_"+std::to_string(t)+".dat";
-			a_vel.open(filename,std::ios::out);
-			filename = "densities_"+std::to_string(t)+".dat";
-			den_file.open(filename,std::ios::out);
-			for(int x=0; x<size_x; x++)
-			{
-				for(int y=0; y<size_y; y++)
+				else
 				{
-					den_file<<x<<"\t"<<y<<"\t"<<rho[y*size_x+x]<<"\n";
-					a_vel<<x<<"\t"<<y<<"\t"<<sqrt(ux[y*size_x+x]*ux[y*size_x+x]+uy[y*size_x+x]*uy[y*size_x+x])<<"\n";
+					// this is a solid node.
+					// here for each n we replace it with the opposite direction.
+					for(int n=0; n<n_connect; n++)
+					{
+						//temp_densities[n_opposite[n]*size_x*size_y+y*size_x+x] = densities[n*size_x*size_y+y*size_x+x];
+						temp_densities[n*size_x*size_y+y*size_x+x] = densities[n_opposite[n]*size_x*size_y+y*size_x+x];
+					}
 				}
 			}
-			a_vel.close();
-			den_file.close();
 		}
 	  //	for(int n=0; n<n_connect; n++)
 	  //	{
@@ -293,10 +331,11 @@ int main()
 		{
 			for(int y=0; y<size_y; y++)
 			{
-				if(solid[y*size_x+x]==0) 
+				//if(solid[y*size_x+x]==0) 
 				{
 					for(int n=0; n<n_connect; n++)
 					{
+						// 		* This was written for half-bounce-back**
 						// Now that we have the updated densities, we can stream the particles. 
 						//
 						// n'th connect indicates the velocity direction. 
@@ -310,59 +349,98 @@ int main()
 						// In this geometry let us consider the top and the bottom borders being 
 						// made of bounce back boundaries and the left and right side with 
 						// periodic boundary conditions.
-						// 
+						//
+
+						/*
+						 * 		THIS IS FOR Full_BOUNCE_BACK
+						 *
+						 * Here the position we are in right now - (x,y) can be solid 
+						 * or not. 
+						 *
+						 * Even a solid node contains populations. 
+						 *
+						 * So we will update the densities of the neighbouring nodes here. 
+						 *
+						 * i.e. We will update (x+en_x,y+en_y) densities here. 
+						 */
 						
 						// with the following code we can identify the PBC along x
-						int x_coord,y_coord; // x_coord and y_coord represent the coordinates from which we will stream the particles to x,y
-						if(x-e_vec_x[n]==size_x)
+						//int x_coord,y_coord; // x_coord and y_coord represent the coordinates from which we will stream the particles to x,y
+
+						int x_coord,y_coord; // x_coord and y_coord represent the coordinates **to** which we will stream the particles **from** x,y
+						if(x+e_vec_x[n]==size_x)
 						{
 							x_coord=0;
 							//y_coord=y-e_vec_y[n];
 						}
-						else if(x-e_vec_x[n]==-1)
+						else if(x+e_vec_x[n]==-1)
 						{
 							x_coord=size_x-1;
 							//y_coord=y-e_vec_y[n];
 						}
 						else
 						{
-							x_coord=x-e_vec_x[n];
+							x_coord=x+e_vec_x[n];
 							//y_coord=y-e_vec_y[n];
 						}
-						if(y-e_vec_y[n]==size_y)
+						if(y+e_vec_y[n]==size_y)
 						{
 							y_coord=0;
 							//y_coord=y-e_vec_y[n];
 						}
-						else if(y-e_vec_y[n]==-1)
+						else if(y+e_vec_y[n]==-1)
 						{
 							y_coord=size_y-1;
 							//y_coord=y-e_vec_y[n];
 						}
 						else
 						{
-							y_coord=y-e_vec_y[n];
+							y_coord=y+e_vec_y[n];
 							//y_coord=y-e_vec_y[n];
 						}
 
-						if(solid[y_coord*size_x+x_coord]) // this means we are trying to stream particles from a solid 
-										  // node, which is not possible. 
-						{
-							densities[n*size_x*size_y+y*size_x+x] = temp_densities[n_opposite[n]*size_x*size_y+y*size_x+x]; // here we are implementing the half-wall reflection 
-																			// boundary condition 
-																			// Since there are not particles that are streaming from 
-																			// the solid wall, we don't have a value for densities 
-																			// for n_connects from the wall to x,y. 
-																			// So we put those densitiy values as particles which got 
-																			// reflected off the wall (...)
-						}
-						else
-						{
-							densities[n*size_x*size_y+y*size_x+x]=temp_densities[n*size_x*size_y+y_coord*size_x+x_coord];
-						}
+						//if(solid[y_coord*size_x+x_coord]) // this means we are trying to stream particles from a solid 
+						//				  // node, which is not possible. 
+						//{
+						//	densities[n*size_x*size_y+y*size_x+x] = temp_densities[n_opposite[n]*size_x*size_y+y*size_x+x]; // here we are implementing the half-wall reflection 
+						//													// boundary condition 
+						//													// Since there are not particles that are streaming from 
+						//													// the solid wall, we don't have a value for densities 
+						//													// for n_connects from the wall to x,y. 
+						//													// So we put those densitiy values as particles which got 
+						//													// reflected off the wall (...)
+						//}
+						//else
+					//	{
+					//		//densities[n*size_x*size_y+y*size_x+x]=temp_densities[n*size_x*size_y+y_coord*size_x+x_coord];
+					//	}
+						densities[n*size_x*size_y+y_coord*size_x+x_coord]=temp_densities[n*size_x*size_y+y*size_x+x];
 					}
 				}
 			}
+		}
+		if(t%write_after==0)
+		{
+			std::cout<<t<<"\n";
+			filename = "average_velocity_"+std::to_string(t)+".dat";
+			a_vel.open(filename,std::ios::out);
+			filename = "densities_"+std::to_string(t)+".dat";
+			den_file.open(filename,std::ios::out);
+			for(int x=0; x<size_x; x++)
+			{
+				for(int y=0; y<size_y; y++)
+				{
+					den_file<<x*(L)<<"\t"<<y*L<<"\t";
+					for(int n=0;n<n_connect; n++)
+					{
+						den_file<<densities[n*size_x*size_y+y*size_x+x]-weights[n]<<"\t";
+					}
+					den_file<<"\n";
+					a_vel<<x<<"\t"<<y<<"\t"<<sqrt(ux[y*size_x+x]*ux[y*size_x+x]+uy[y*size_x+x]*uy[y*size_x+x])<<"\t"<<ux[y*size_x+x]<<"\t"<<uy[y*size_x+x]<<"\n";
+				}
+			}
+			a_vel.close();
+			den_file.close();
 		}
 	}
 	//for(int x=0; x<size_x; x++)
